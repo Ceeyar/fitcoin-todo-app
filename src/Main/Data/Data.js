b9fac79d26cbbde9ab3fb1796f76699a677240e4
Siyabonga Ndovela <201490428@student.uj.ac.za>
2020-10-14 02:51:22 +0200

    Organised data into coed segments

---
+++ src/Main/Data/Data.js
@@ -0,0 +1,18 @@
+
+export const todoData = [
+    {
+      id: 0,
+      text: "Meditate",
+      isCompleted: false
+    },
+    {
+      id: 1,
+      text: "Stay Happy",
+      isCompleted: false
+    },
+    {
+      id: 2,
+      text: "Drink Enough Water",
+      isCompleted: false
+    }
+  ]
